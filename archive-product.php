1ab21a35f93015b910a1f8de693be91a200e8610
nafas <nafas@gmail.com>
2026-08-30 11:42:51 +0330

    taxonomy/Day23

--- archive-product.php
+++ archive-product.php
@@ -2,8 +2,33 @@
 
 <main>
 
-    <h1>محصولات</h1>
+    <!-- نمایش دسته بندی محصولات -->
+     <h2>دسته‌بندی محصولات</h2>
 
+<?php
+$categories = get_terms(
+    array(
+        'taxonomy' => 'product_category',
+        'hide_empty' => true,
+    )
+);
+?>
+
+<ul>
+
+    <?php foreach ( $categories as $category ) : ?>
+
+        <li>
+            <a href="<?php echo get_term_link( $category ); ?>">
+                <?php echo $category->name; ?>
+            </a>
+        </li>
+
+    <?php endforeach; ?>
+
+</ul>
+    
+    <h1>محصولات</h1>
     <?php while ( have_posts() ) : the_post(); ?>
 
         <article>
